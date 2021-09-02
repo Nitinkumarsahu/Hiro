d8bd2d3f928f38aed34afc62b4bddbf6da5424ff
Nakshatra <87998363+Nakshatra4762@users.noreply.github.com>
2021-09-02 20:42:44 +0530

    Create test.ts

---
+++ src/commands/Fun/test.ts
@@ -0,0 +1,40 @@
+import { MessageType } from '@adiwajshing/baileys'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import request from '../../lib/request'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+import  axios  from 'axios'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'neko',
+            description: 'Will provide a random anime Neko images',
+            category: 'weeb',
+            usage: `${client.config.prefix}neko`,
+            
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+
+
+        const rnekol = ["hug"];
+        const rnekolc = rnekol[Math.floor(Math.random() * rnekol.length)];
+        const neko = await axios.get('https://yuabot.com/weeb/api/v1/img/' + rnekolc)
+
+return void M.reply(await request.buffer(neko.data.url), MessageType.image, undefined, undefined, `*Nyanpasu ~~*`)
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
