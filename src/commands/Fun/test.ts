2591d1364bef2a83b85c109624706b6fcb8a00b4
Nakshatra <87998363+Nakshatra4762@users.noreply.github.com>
2021-09-02 22:07:32 +0530

    Update test.ts

--- src/commands/Fun/test.ts
+++ src/commands/Fun/test.ts
@@ -18,22 +18,30 @@ export default class Command extends BaseCommand {
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
+        
+        const user1 = M.sender.jid
+        const user2 = M.mentioned[0]
+//  let username1 = user1.split('@')[0]
+//  let username2 = user2.split('@')[0]
+// let username1 = user1.replace('@s.whatsapp.net', '')
+// let username2 = user2.replace('@s.whatsapp.net', '')
+
 
 
         const rnekol = ["hug"];
         const rnekolc = rnekol[Math.floor(Math.random() * rnekol.length)];
         const neko = await axios.get('https://yuabot.com/weeb/api/v1/img/' + rnekolc)
 
-return void M.reply(await request.buffer(neko.data.payload.url), MessageType.video, )
+return void M.reply(await request.buffer(neko.data.payload.url), MessageType.video, 
+                   
+                   
+                   { mimetype: Mimetype.gif, caption: `@${user1.split('@')[0]} hugged @${user2.split('@')[0]}`, contextInfo: {mentionedJid : [user1, user2]} }
+                   
+                   
+                   )
         
         
-        
-        { mimetype: Mimetype.gif }
-        
-    
-
-
-
+       
     }
 
 
