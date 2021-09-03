cf2257062370f16a8e9d7e32d0e0b269775fe68c
Nakshatra <87998363+Nakshatra4762@users.noreply.github.com>
2021-09-03 10:15:39 +0530

    Update test.ts

--- src/commands/Fun/test.ts
+++ src/commands/Fun/test.ts
@@ -26,7 +26,7 @@ export default class Command extends BaseCommand {
 
 return void M.reply(await request.buffer(neko.data.payload.url), 
                     
-                    MessageType.video, 
+                    MessageType.video 
                    
                    
                    mimetype: Mimetype.gif 
